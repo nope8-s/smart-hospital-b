fb7a23f49bd7dc18bcdaed3a1fd797bf39f4b65c
nope8-s <ben.david.emm@gmail.com>
2026-07-02 11:36:32 +0700

    Update hospital_app.py

--- hospital_app.py
+++ hospital_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import pickle
 
-with open("hospital_model.pkl, "rb") as f:
+with open("hospital_model.pkl", "rb") as f:
           bundle = pickle.load(f)
           st.write ("Connected")
 
